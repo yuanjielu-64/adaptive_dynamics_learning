89e52fa748ab87ab0d804c0ffcaf7b615719b607
yuanjielu <ylu22@gmu.edu>
2025-08-13 16:36:53 -0400

    first commit

--- ros_jackal/singularity_run_cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/lusr/opt/apptainer/bin
-export ROS_HOSTNAME=localhost
-export ROS_MASTER_URI=http://localhost:11311
-# export IMAGE_PATH=/scratch/cluster/zifan/ros_jackal_image.sif
-
-apptainer exec -i --nv -n --network=none -p -B /var/condor:/var/condor -B `pwd`:/jackal_ws/src/ros_jackal -B ${BUFFER_PATH}:${BUFFER_PATH} ${IMAGE_PATH} /bin/bash /jackal_ws/src/ros_jackal/entrypoint.sh ${@:1}
